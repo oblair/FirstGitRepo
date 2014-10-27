1ff1df4a44d02bead2515f52789a4f13e061d356
Tim <zealnz@gmail.com>
2014-10-27 20:29:10 +1300

    Fade to red
    Starting

--- engine.js
+++ engine.js
@@ -3,12 +3,20 @@ var game = {
     score:0,
     danger_card:false,
     increaseScore:function(value){
-        console.log('Score: ' + this.score + ' + ' + value + ' = ' + this.score + value);
+        //console.log('Score: ' + this.score + ' + ' + value + ' = ' + this.score + value);
         this.score += value;
-        $('.container h1').html(this.score).css({'font-size':((this.score/10)+1)+'em'});
+        $('.container h1')
+            .html(this.score)
+            .css({'font-size':((this.score/10)+2)+'em'})
+            .animate({'font-size':((this.score/10)+1)+'em'},{
+                queue:false,
+                easing:'swing',
+                duration:100
+
+            });
     },
     resetScore:function(){
-        console.log('Resetting score.');
+        //console.log('Resetting score.');
         this.score = 0;
         $('.container h1').html(this.score);
         console.log('Resetting score.');
@@ -19,10 +27,15 @@ $(document).ready(function(){
     console.log('engine.js loaded');
     init();
     //setInterval(changeMode, 2000);
-    $(document).click(clickCheck);
+    //$(document).click(clickCheck);
+    $(document).on('touchstart click', function(e){
+        e.preventDefault();
+        clickCheck();
+    })
 });
 
 function clickCheck(){
+    console.log(jQuery.Color($('.container'),'background-color').hue());
     if(game.danger_card){
         game.resetScore();
         //makeCard();
@@ -35,13 +48,36 @@ function clickCheck(){
 function makeCard(){
     window.clearTimeout(next_timer);
     var random_num = Math.ceil(Math.random()*255);
-    console.log('New card: ' + random_num);
-    if(random_num < 15 || random_num > 345){
-        game.danger_card = true;
+    var interpolate_to;
+    var colour_dist;
+    //console.log('New card: ' + random_num);
+    // if(random_num < 15 || random_num > 345){
+    //     game.danger_card = true;
+    // }else{
+    //     game.danger_card = false;
+    // }
+
+
+
+    if(random_num < 180){
+        interpolate_to = 0;
+        colour_dist = random_num;
     }else{
-        game.danger_card = false;
+        interpolate_to = 360;
+        colour_dist = 360 - random_num;
     }
-    $('.container').css({'background-color': 'hsla('+random_num+',50%,50%,1)'});
+    console.log(colour_dist);
+    $('.container')
+        .css({'background-color': 'hsla('+random_num+',50%,50%,1)'})
+        .animate({'background-color': 'hsla('+interpolate_to+',50%,50%,1)'},{
+            queue:false,
+            easing:'linear',
+            duration:colour_dist*50,
+            progress:function(animation,progress,remainingMs){
+                //console.log(colour_dist);
+                // console.log(remainingMs);
+            }
+        });
     next_timer = setTimeout(timeout,2000,1);
 }
 
